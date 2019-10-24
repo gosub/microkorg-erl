af20a093356d4684718e39bc135d9521d5a4fefe
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2019-10-24 12:37:03 +0200

    completed 'program:to_map' function definition

--- program.erl
+++ program.erl
@@ -8,17 +8,19 @@ to_map(ProgramData) ->
     <<Name:12/bytes, _:16, ArpCtrlData:2/bytes,
       _:2, VoiceMode:2, 0:4, ScaleKey:4, ScaleType:4, _:8,
       DelayFx:4/bytes, ModFx:3/bytes, Eq:4/bytes, Arp:7/bytes,
-      Rest/bytes>> = ProgramData,
+      KbdOctave/signed-integer, VoicesParams:216/bytes>> = ProgramData,
+    Mode = voice_mode_from_int(VoiceMode),
     #{name => Name,
       arpctrl => arpctrl_to_map(ArpCtrlData),
-      voice_mode => voice_mode_from_int(VoiceMode),
+      voice_mode => Mode,
       scale_key => scale_key_from_int(ScaleKey),
       scale_type => ScaleType,
       delayfx => delayfx_to_map(DelayFx),
       modfx => modfx_to_map(ModFx),
       eq => eq_to_map(Eq),
       arp => arp_to_map(Arp),
-      rest => Rest}.
+      kbd_oct => KbdOctave,
+      voices => voices_to_list(Mode, VoicesParams)}.
 
 arpctrl_to_map(<<0:5, Len:3, T1:1,T2:1,T3:1,T4:1,T5:1,T6:1,T7:1,T8:1>>) ->
     #{len => Len+1, pattern => [T1,T2,T3,T4,T5,T6,T7,T8]}.
