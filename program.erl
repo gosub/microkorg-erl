33770c6748969143159a09b71cb9f3b2c06ac069
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2019-10-24 12:48:07 +0200

    eq parameters decoding

--- program.erl
+++ program.erl
@@ -43,8 +43,6 @@ delay_timebase_from_int(N) ->
     lists:nth(N+1, ['1/32','1/24','1/16','1/12','3/32','1/8','1/6',
 		    '3/16','1/4','1/3','3/8','1/2','2/3','3/4','1/1']).
 
-eq_to_map(<<>>) ->
-    todo.
 delay_type_from_int(0) -> stereo;
 delay_type_from_int(1) -> cross;
 delay_type_from_int(2) -> lr.
@@ -59,3 +57,24 @@ modfx_to_map(<<LFOSpeed:8,Depth:8,Type:8>>) ->
 mod_type_from_int(0) -> chorus;
 mod_type_from_int(1) -> ensemble;
 mod_type_from_int(2) -> phaser.
+
+eq_to_map(<<HiFreq:8,HiGain:8,LoFreq:8,LoGain:8>>) ->
+    #{hifreq => hifreq_from_int(HiFreq),
+     higain => gain_from_int(HiGain),
+     lofreq => lofreq_from_int(LoFreq),
+     logain => gain_from_int(LoGain)}.
+
+hifreq_from_int(N) ->
+    lists:nth(N+1,[1000, 1250, 1500, 1750, 2000, 2250, 2500, 2750, 3000,
+		  3250, 3500, 3750, 4000, 4250, 4500, 4750, 5000, 5250,
+		  5500, 5750, 6000, 7000, 8000, 9000, 10000, 11000, 12000,
+		  14000, 16000, 18000]).
+
+lofreq_from_int(N) ->
+    lists:nth(N+1, [40, 50, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240,
+		   260, 280, 300, 320, 340, 360, 380, 400, 420, 440, 460,
+		   480, 500, 600, 700, 800, 900, 1000]).
+
+gain_from_int(N) when N >= 64-12, N =< 64+12 ->
+    N-64.
+
