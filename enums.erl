e4de938062c79c4534af9a25e8198d4365c227ac
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2019-10-29 10:05:10 +0100

    redefined 'enums:scale_key' in terms of 'utils:list_to_fun'

--- enums.erl
+++ enums.erl
@@ -1,6 +1,10 @@
 -module(enums).
 -export([onoff/1, voice_mode/1, scale_key/1]).
 
+-define(SCALE_KEY, ['C','C#','D','D#','E','F',
+		    'F#','G','G#','A','A#','B']).
+
+
 onoff(0) -> off;
 onoff(1) -> on;
 onoff(off) -> 0;
@@ -13,27 +17,7 @@ voice_mode(single)  -> 0;
 voice_mode(double)  -> 2;
 voice_mode(vocoder) -> 3.
 
-scale_key(0) -> 'C';
-scale_key(1) -> 'C#';
-scale_key(2) -> 'D';
-scale_key(3) -> 'D#';
-scale_key(4) -> 'E';
-scale_key(5) -> 'F';
-scale_key(6) -> 'F#';
-scale_key(7) -> 'G';
-scale_key(8) -> 'G#';
-scale_key(9) -> 'A';
-scale_key(10) -> 'A#';
-scale_key(11) -> 'B';
-scale_key('C')  -> 0;
-scale_key('C#') -> 1;
-scale_key('D')  -> 2;
-scale_key('D#') -> 3;
-scale_key('E')  -> 4;
-scale_key('F')  -> 5;
-scale_key('F#') -> 6;
-scale_key('G')  -> 7;
-scale_key('G#') -> 8;
-scale_key('A')  -> 9;
-scale_key('A#') -> 10;
-scale_key('B')  -> 11.
+
+scale_key(X) ->
+    F = utils:list_to_fun(?SCALE_KEY),
+    F(X).
