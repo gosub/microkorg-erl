1ba93892daa0292779a00e13de58adb4fa8c751b
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2019-11-05 11:28:37 +0100

    added 'read_file' function to 'program' module

--- program.erl
+++ program.erl
@@ -1,8 +1,12 @@
 -module(program).
--export([to_map/1, from_map/1]).
+-export([to_map/1, from_map/1, read_file/1]).
 
 to_map(ProgramData) ->
     program_decode:to_map(ProgramData).
 
 from_map(ProgramMap) ->
     program_encode:from_map(ProgramMap).
+
+read_file(SysexFile) ->
+    ProgramData = sysex:decode_file(SysexFile),
+    to_map(ProgramData).
