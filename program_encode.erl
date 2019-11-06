8e890bc7e3616dcbac4e7648d3c62dcc2823eb82
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2019-11-06 10:57:55 +0100

    removed comment with program:to_map as reference not needed anymore

--- program_encode.erl
+++ program_encode.erl
@@ -18,24 +18,6 @@ from_map(#{name:=Name, arpctrl:=ArpCtrl, voice_mode:=VoiceMode,
       DelayFxData:4/bytes, ModFxData:3/bytes, EqData:4/bytes,
       ArpData:7/bytes, KbdOctave/signed-integer, VoicesData:216/bytes>>.
 
-%% to_map(ProgramData) ->
-%%     <<Name:12/bytes, _:16, ArpCtrlData:2/bytes,
-%%       _:2, VoiceMode:2, 0:4, ScaleKey:4, ScaleType:4, _:8,
-%%       DelayFx:4/bytes, ModFx:3/bytes, Eq:4/bytes, Arp:7/bytes,
-%%       KbdOctave/signed-integer, VoicesParams:216/bytes>> = ProgramData,
-%%     Mode = enums:voice_mode(VoiceMode),
-%%     #{name => Name,
-%%       arpctrl => arpctrl(ArpCtrlData),
-%%       voice_mode => Mode,
-%%       scale_key => enums:scale_key(ScaleKey),
-%%       scale_type => ScaleType,
-%%       delayfx => delayfx(DelayFx),
-%%       modfx => modfx(ModFx),
-%%       eq => eq(Eq),
-%%       arp => arp(Arp),
-%%       kbd_oct => KbdOctave,
-%%       voices => voices_to_list(Mode, VoicesParams)}.
-
 arpctrl(#{len:=Len, pattern:=[T1,T2,T3,T4,T5,T6,T7,T8]}) ->
     LenDec = Len - 1,
     <<0:5, LenDec:3, T1:1,T2:1,T3:1,T4:1,T5:1,T6:1,T7:1,T8:1>>.
