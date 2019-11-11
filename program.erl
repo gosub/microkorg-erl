2600f2bafe9858e52571a60d28850ddd793b43ae
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2019-11-11 12:34:11 +0100

    implemented interface 'program:write_file'

--- program.erl
+++ program.erl
@@ -1,5 +1,5 @@
 -module(program).
--export([to_map/1, from_map/1, read_file/1]).
+-export([to_map/1, from_map/1, read_file/1, write_file/2]).
 
 to_map(ProgramData) ->
     program_decode:to_map(ProgramData).
@@ -10,3 +10,7 @@ from_map(ProgramMap) ->
 read_file(SysexFile) ->
     ProgramData = sysex:decode_file(SysexFile),
     to_map(ProgramData).
+
+write_file(SysexFile, ProgramMap) ->
+    ProgramData = from_map(ProgramMap),
+    sysex:encode_file(SysexFile, ProgramData).
