0347e9b347695850f9146dd417f4640e50e9188a
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2019-11-22 12:07:46 +0100

    'program:write_random/0' generates a random sysex patch and its name

--- program.erl
+++ program.erl
@@ -1,6 +1,6 @@
 -module(program).
 -export([to_map/1, from_map/1, read_file/1, write_file/2,
-	 random/0, write_random/1]).
+	 random/0, write_random/1, write_random/0]).
 
 to_map(ProgramData) ->
     program_decode:to_map(ProgramData).
@@ -21,3 +21,10 @@ random() ->
 
 write_random(SysexFile) ->
     write_file(SysexFile, random()).
+
+write_random() ->
+    Program = random(),
+    #{name := BinName} = Program,
+    Name = string:strip(binary_to_list(BinName)) ++ ".syx",
+    Res = write_random(Name),
+    {Res, Name}.
