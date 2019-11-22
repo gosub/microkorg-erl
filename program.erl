359f49b8402480e9850cb1c3567b2c616ff56662
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2019-11-22 12:28:49 +0100

    write_file/1 to write a patch file deriving the name from the map

--- program.erl
+++ program.erl
@@ -1,5 +1,5 @@
 -module(program).
--export([to_map/1, from_map/1, read_file/1, write_file/2,
+-export([to_map/1, from_map/1, read_file/1, write_file/2, write_file/1,
 	 random/0, write_random/1, write_random/0, set_name/2]).
 
 to_map(ProgramData) ->
@@ -15,6 +15,11 @@ read_file(SysexFile) ->
 write_file(SysexFile, ProgramMap) ->
     ProgramData = from_map(ProgramMap),
     sysex:encode_file(SysexFile, ProgramData).
+
+write_file(ProgramMap) ->
+    #{name := Name} = ProgramMap,
+    Filename = string:strip(binary_to_list(Name)) ++ ".syx",
+    write_file(Filename, ProgramMap).
 
 random() ->
     program_random:generate().
