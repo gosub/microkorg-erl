6a857be953abc306073ebb29bba6165b44df839d
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2019-11-24 10:02:28 +0100

    'sysex:request/1' defaults to midi channel 0

--- sysex.erl
+++ sysex.erl
@@ -1,7 +1,7 @@
 -module(sysex).
 -export([trim/1, untrim/1, scramble/1, unscramble/1,
 	encode/1, decode/1, encode_file/2, decode_file/1,
-	request/2]).
+	request/1, request/2]).
 
 
 trim(Sysex) ->
@@ -97,6 +97,9 @@ encode_file(SysexFile, Data) ->
 
 
 % MIDI REQUEST MESSAGES
+
+request(Request) ->
+    request(Request, 0).
 
 request(device_inquiry, MidiCh) -> <<16#F07E:16, MidiCh:8, 16#0601F7:24>>;
 request(current_program_data_dump, MidiCh) -> generic_request(16#10, MidiCh);
