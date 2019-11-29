1444b6eb7a459bd389c5636e72e0cfc4e43309b7
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2019-11-29 13:25:24 +0100

    'all_programs:read_file' returns a list of 'ProgramMap's

--- all_programs.erl
+++ all_programs.erl
@@ -1,5 +1,13 @@
 -module(all_programs).
 -export([read_file/1]).
 
+
 read_file(SysexFile) ->
-    {program_data_dump, AllProgsData} = sysex:decode_file(SysexFile).
+    {program_data_dump, AllProgsData} = sysex:decode_file(SysexFile),
+    lists:map(fun program:to_map/1, separate_programs(AllProgsData)).
+
+
+separate_programs(<<>>) ->
+    [];
+separate_programs(<<Program:254/bytes,Rest/bytes>>) ->
+    [Program | separate_programs(Rest)].
