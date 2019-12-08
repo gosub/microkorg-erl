f9d5049b3dea96df3af9802fc767ae0a876ad1fd
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2019-12-08 14:23:04 +0100

    added 'eg2' to 'program_encode:vocoder'

--- program_encode.erl
+++ program_encode.erl
@@ -200,7 +200,8 @@ vocoder(#{midi_ch:=MidiCh, assign_mode:=AssignMode, eg2_reset:=EG2Reset,
       (vocoder_filter(Filter)):6/bytes,
       (vocoder_amp(Amp)):5/bytes,
       0:8, 0:8, 127:8, 0:8,
-      0:(68*8)>>.
+      (eg(EG2)):4/bytes,
+      0:(64*8)>>.
 
 vocoder_mixer(#{osc1_lvl := Osc1Lvl,
 		ext1_lvl := Ext1Lvl,
