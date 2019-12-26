7e01b89a66bc3b676d0e141e28e7c8e3986355f6
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2019-12-26 11:09:42 +0100

    added 'write_file/2' to 'all_programs' module

--- all_programs.erl
+++ all_programs.erl
@@ -1,10 +1,16 @@
 -module(all_programs).
--export([read_file/1]).
+-export([read_file/1, write_file/2]).
 
 
 read_file(SysexFile) ->
     {program_data_dump, AllProgsData} = sysex:decode_file(SysexFile),
     lists:map(fun program:to_map/1, separate_programs(AllProgsData)).
+
+
+write_file(ProgramList, SysexFile) when length(ProgramList) == 128 ->
+    Binaries = lists:map(fun program:from_map/1, ProgramList),
+    Binary = list_to_binary(Binaries),
+    sysex:encode_file(SysexFile, program_data_dump, Binary).
 
 
 separate_programs(<<>>) ->
