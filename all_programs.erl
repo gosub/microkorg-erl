5671f25c007bcd22060e3e2a251bcaea78749a3d
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2019-12-27 10:19:03 +0100

    added 'random' to 'all_programs' module

--- all_programs.erl
+++ all_programs.erl
@@ -1,5 +1,5 @@
 -module(all_programs).
--export([read_file/1, write_file/2]).
+-export([read_file/1, write_file/2, random/0]).
 
 
 read_file(SysexFile) ->
@@ -11,6 +11,12 @@ write_file(ProgramList, SysexFile) when length(ProgramList) == 128 ->
     Binaries = lists:map(fun program:from_map/1, ProgramList),
     Binary = list_to_binary(Binaries),
     sysex:encode_file(SysexFile, program_data_dump, Binary).
+
+
+random() ->
+    NonVocoders = [program:random() || _ <- lists:seq(1, (128-16))],
+    Vocoders = [program:random_vocoder() || _ <-lists:seq(1, 16)],
+    NonVocoders ++ Vocoders.
 
 
 separate_programs(<<>>) ->
