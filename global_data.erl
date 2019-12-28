f8ab6c38dca25628bca476816b8d666a6133c975
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2019-12-28 12:24:27 +0100

    added 'local_ctrl' and 'memory_protect' to 'global_data:to_map'

--- global_data.erl
+++ global_data.erl
@@ -11,14 +11,17 @@ to_map(<<MasterTune:8/signed-integer,
 	 Transpose:8/signed-integer,
 	 0:7, Position:1,
 	 VelValue:8, VelCurve:8,
-	 _Rest:195/bytes>>)
+	 _:5, LocalCtrl:1, _:1, MemoryProtect:1,
+	 _Rest:194/bytes>>)
   when VelValue >= 1, VelValue =< 127,
        VelCurve =< 8 ->
     #{master_tune => MasterTune/10 + 440,
       transpose => Transpose,
       position => position(Position),
       vel_value => VelValue,
-      vel_curve => velcurve(VelCurve)}.
+      vel_curve => velcurve(VelCurve),
+      local_ctrl => enums:onoff(LocalCtrl),
+      memory_protect => enums:onoff(MemoryProtect)}.
 
 position(0) -> postkbd;
 position(1) -> pretg.
