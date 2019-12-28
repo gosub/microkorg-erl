aea4b79a226cd5a2ee73716d2cd429738e30a4a5
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2019-12-28 12:20:19 +0100

    added 'vel_value' and 'vel_curve' to 'global_data:to_map'

--- global_data.erl
+++ global_data.erl
@@ -10,10 +10,18 @@ read_file(SysexFile) ->
 to_map(<<MasterTune:8/signed-integer,
 	 Transpose:8/signed-integer,
 	 0:7, Position:1,
+	 VelValue:8, VelCurve:8,
 	 _Rest:195/bytes>>)
+  when VelValue >= 1, VelValue =< 127,
+       VelCurve =< 8 ->
     #{master_tune => MasterTune/10 + 440,
       transpose => Transpose,
-      position => position(Position)}.
+      position => position(Position),
+      vel_value => VelValue,
+      vel_curve => velcurve(VelCurve)}.
 
 position(0) -> postkbd;
 position(1) -> pretg.
+
+velcurve(8) -> const;
+velcurve(X) -> X+1.
