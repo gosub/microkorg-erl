c3b3f9e17ab94989ef6b5542078384a11b7b995a
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2019-12-29 09:41:11 +0100

    added 'clock' to 'global_data:to_map'

--- global_data.erl
+++ global_data.erl
@@ -12,7 +12,8 @@ to_map(<<MasterTune:8/signed-integer,
 	 0:7, Position:1,
 	 VelValue:8, VelCurve:8,
 	 _:5, LocalCtrl:1, _:1, MemoryProtect:1,
-	 _Rest:194/bytes>>)
+	 _:16, 0:6, Clock:2,
+	 _Rest:191/bytes>>)
   when VelValue >= 1, VelValue =< 127,
        VelCurve =< 8 ->
     #{master_tune => MasterTune/10 + 440,
@@ -21,10 +22,15 @@ to_map(<<MasterTune:8/signed-integer,
       vel_value => VelValue,
       vel_curve => velcurve(VelCurve),
       local_ctrl => enums:onoff(LocalCtrl),
-      memory_protect => enums:onoff(MemoryProtect)}.
+      memory_protect => enums:onoff(MemoryProtect),
+      clock => clock(Clock)}.
 
 position(0) -> postkbd;
 position(1) -> pretg.
 
 velcurve(8) -> const;
 velcurve(X) -> X+1.
+
+clock(0) -> internal;
+clock(1) -> external;
+clock(2) -> auto.
