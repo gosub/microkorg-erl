c76c9742ac5629ae32e292493b02197b0804cd02
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2020-01-05 19:09:03 +0100

    added 'to_map/1' function to 'all_data' module

--- all_data.erl
+++ all_data.erl
@@ -1,7 +1,10 @@
 -module(all_data).
--export([read_file/1]).  % to_map/1, from_map/1, read_file/1, write_file/2]).
+-export([read_file/1, to_map/1]).  % from_map/1, write_file/2]).
 
 read_file(SysexFile) ->
     {all_data_dump, AllData} = sysex:decode_file(SysexFile),
     to_map(AllData).
 
+to_map(<<AllProgramData:32512/bytes, GlobalData:200/bytes>>) ->
+    #{programs => all_programs:to_map(AllProgramData),
+      global_data => global_data:to_map(GlobalData)}.
