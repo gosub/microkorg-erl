ff9b1e2425f129ae7657a06c07df60b6ff447525
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2020-01-05 19:08:08 +0100

    added 'read_file/1' function to 'all_data' module

--- all_data.erl
+++ all_data.erl
@@ -1,2 +1,7 @@
 -module(all_data).
--export([]).  % to_map/1, from_map/1, read_file/1, write_file/2]).
+-export([read_file/1]).  % to_map/1, from_map/1, read_file/1, write_file/2]).
+
+read_file(SysexFile) ->
+    {all_data_dump, AllData} = sysex:decode_file(SysexFile),
+    to_map(AllData).
+
