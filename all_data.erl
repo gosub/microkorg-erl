ea1fb515c2b563bbd61d33ade8cd3aee22ae01ef
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2020-01-06 15:32:40 +0100

    added 'from_map' fun to 'all_data' module

--- all_data.erl
+++ all_data.erl
@@ -1,5 +1,5 @@
 -module(all_data).
--export([read_file/1, to_map/1]).  % from_map/1, write_file/2]).
+-export([read_file/1, to_map/1, from_map/1]).  % write_file/2]).
 
 read_file(SysexFile) ->
     {all_data_dump, AllData} = sysex:decode_file(SysexFile),
@@ -8,3 +8,7 @@ read_file(SysexFile) ->
 to_map(<<AllProgramData:32512/bytes, GlobalData:200/bytes>>) ->
     #{programs => all_programs:to_list(AllProgramData),
       global_data => global_data:to_map(GlobalData)}.
+
+from_map(#{global_data := GD, programs := Prgs}) ->
+    <<(all_programs:from_list(Prgs)):32512/bytes,
+      (global_data:from_map(GD)):200/bytes>>.
